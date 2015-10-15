23997fe6d56c3b1d21e5d545c672a6d1a84b713c
neo <liang.gong@gmail.com>
2015-10-15 19:23:16 +0800

    1.0.0 tree

--- dist/neo-tree.js
+++ dist/neo-tree.js
@@ -5,12 +5,16 @@
 'use strict';
 var Tree=require('./lib/tree');
 var TreeIterator=require('./lib/tree-iterator');
+var DefaultTreeNodeInterpreter=require('./lib/default-tree-node-interpreter');
+var TreeNode=require('./lib/tree-node');
 module.exports.Tree=Tree;
 module.exports.TreeIterator=TreeIterator;
+module.exports.DefaultTreeNodeInterpreter=DefaultTreeNodeInterpreter;
+module.exports.TreeNode=TreeNode;
 
 
 
-},{"./lib/tree":5,"./lib/tree-iterator":3}],2:[function(require,module,exports){
+},{"./lib/default-tree-node-interpreter":2,"./lib/tree":5,"./lib/tree-iterator":3,"./lib/tree-node":4}],2:[function(require,module,exports){
 /**
  * Created by neo on 2015/10/9.
  */
--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 'use strict';
 var Tree=require('./lib/tree');
 var TreeIterator=require('./lib/tree-iterator');
+var DefaultTreeNodeInterpreter=require('./lib/default-tree-node-interpreter');
+var TreeNode=require('./lib/tree-node');
 module.exports.Tree=Tree;
 module.exports.TreeIterator=TreeIterator;
+module.exports.DefaultTreeNodeInterpreter=DefaultTreeNodeInterpreter;
+module.exports.TreeNode=TreeNode;
 
 
